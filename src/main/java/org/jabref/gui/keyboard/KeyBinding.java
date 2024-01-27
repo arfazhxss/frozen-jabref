acf1ab1b3f464a85f66bcbb8ae4c04e996852a7c
Arfaz Hussain <arfazhussain.zn@gmail.com>
2024-01-26 16:02:49 -0800

    Lab Initialization (January 26, 2024) Commit by @arfazhxss on Fri 26 Jan 2024

--- src/main/java/org/jabref/gui/keyboard/KeyBinding.java
+++ src/main/java/org/jabref/gui/keyboard/KeyBinding.java
@@ -28,7 +28,7 @@ public enum KeyBinding {
     AUTOMATICALLY_LINK_FILES("Automatically link files", Localization.lang("Automatically set file links"), "F7", KeyBindingCategory.QUALITY),
     CHECK_INTEGRITY("Check integrity", Localization.lang("Check integrity"), "ctrl+F8", KeyBindingCategory.QUALITY),
     CLEANUP("Cleanup", Localization.lang("Cleanup entries"), "alt+F8", KeyBindingCategory.QUALITY),
-    CLOSE_DATABASE("Close library", Localization.lang("Close library"), "ctrl+W", KeyBindingCategory.FILE),
+    CLOSE_DATABASE("Close library", Localization.lang("Close library"), "ctrl+shift+W", KeyBindingCategory.FILE),
     CLOSE("Close dialog", Localization.lang("Close dialog"), "Esc", KeyBindingCategory.VIEW),
     COPY("Copy", Localization.lang("Copy"), "ctrl+C", KeyBindingCategory.EDIT),
     COPY_TITLE("Copy title", Localization.lang("Copy title"), "ctrl+shift+alt+T", KeyBindingCategory.EDIT),
