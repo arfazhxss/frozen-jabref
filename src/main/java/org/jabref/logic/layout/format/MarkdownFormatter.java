27a09ac877f2a6a97292ba0729f2499a9aa112c7
Arfaz Hussain <arfazhussain.zn@gmail.com>
2024-02-15 22:40:16 -0800

    Minor Update (MarkdownFormatter) - Issue #10558 Commit by @arfazhxss on Thu 15 Feb 2024

--- src/main/java/org/jabref/logic/layout/format/MarkdownFormatter.java
+++ src/main/java/org/jabref/logic/layout/format/MarkdownFormatter.java
@@ -1,33 +1,75 @@
+// package org.jabref.logic.layout.format;
+
+// import java.util.Objects;
+
+// import org.jabref.logic.layout.LayoutFormatter;
+
+// import com.vladsch.flexmark.html2md.converter.FlexmarkHtmlConverter;
+// import com.vladsch.flexmark.parser.Parser;
+// import com.vladsch.flexmark.util.ast.Node;
+// import com.vladsch.flexmark.util.data.MutableDataSet;
+
+// public class MarkdownFormatter implements LayoutFormatter {
+
+//     private final Parser parser;
+//     private final FlexmarkHtmlConverter renderer;
+
+//     public MarkdownFormatter() {
+//         MutableDataSet options = new MutableDataSet();
+//         parser = Parser.builder(options).build();
+//         renderer = FlexmarkHtmlConverter.builder().build();
+//     }
+
+//     @Override
+//     public String format(final String fieldText) {
+//         Objects.requireNonNull(fieldText, "Field Text should not be null, when handed to formatter");
+
+//         Node document = parser.parse(fieldText);
+//         String html = renderer.convert(document.toString());
+//         // return html.replaceAll("\\r\\n|\\r|\\n", " ").trim();
+//         return "testcheck";
+//     }
+// }
+
 package org.jabref.logic.layout.format;
 
 import java.util.Objects;
 
 import org.jabref.logic.layout.LayoutFormatter;
 
+// import com.vladsch.flexmark.html.HtmlRenderer;
+// import com.vladsch.flexmark.parser.Parser;
+// import com.vladsch.flexmark.util.ast.Node;
+// import com.vladsch.flexmark.util.data.MutableDataSet;
 import com.vladsch.flexmark.html2md.converter.FlexmarkHtmlConverter;
-import com.vladsch.flexmark.parser.Parser;
-import com.vladsch.flexmark.util.ast.Node;
-import com.vladsch.flexmark.util.data.MutableDataSet;
 
 public class MarkdownFormatter implements LayoutFormatter {
 
-    private final Parser parser;
-    private final FlexmarkHtmlConverter renderer;
+    // private final Parser parser;
+    // private final HtmlRenderer renderer;
 
-    public MarkdownFormatter() {
-        MutableDataSet options = new MutableDataSet();
-        parser = Parser.builder(options).build();
-        renderer = FlexmarkHtmlConverter.builder().build();
-    }
+    // public MarkdownFormatter() {
+    //     MutableDataSet options = new MutableDataSet();
+    //     parser = Parser.builder(options).build();
+    //     renderer = HtmlRenderer.builder(options).build();
+    // }
 
     @Override
     public String format(final String fieldText) {
         Objects.requireNonNull(fieldText, "Field Text should not be null, when handed to formatter");
 
-        Node document = parser.parse(fieldText);
-        String html = renderer.convert(document.toString());
-        // return html.replaceAll("\\r\\n|\\r|\\n", " ").trim();
-        return "testcheck";
+        String md = Html2Markdown(fieldText); // Calling html to markdown conversion method
+        // Node document = parser.parse(fieldText);
+        // String html = renderer.render(document);
 
+        // workaround HTMLChars transforming "\n" into <br> by returning a one liner
+        // return md.replaceAll("\\r\\n|\\r|\\n", " ").trim();
+        return md;
     }
+
+    // new method to convert html to markdown
+    public String Html2Markdown(String html) {
+        return FlexmarkHtmlConverter.builder().build().convert(html);
+    }
+
 }
