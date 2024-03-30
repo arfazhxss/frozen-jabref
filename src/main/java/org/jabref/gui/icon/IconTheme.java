bd756ff62a3b5cf8369a8460e3293c90a1b3b477
dhuruvan13 <145606952+dhuruvan13@users.noreply.github.com>
2024-03-29 17:55:06 -0700

    Update IconTheme.java

--- src/main/java/org/jabref/gui/icon/IconTheme.java
+++ src/main/java/org/jabref/gui/icon/IconTheme.java
@@ -229,10 +229,10 @@ public class IconTheme {
         AUTO_FILE_LINK(MaterialDesignF.FILE_FIND),
         AUTO_RENAME(MaterialDesignA.AUTO_FIX),
         DOWNLOAD_FILE(MaterialDesignD.DOWNLOAD),
-// TODO: For A3.3 Icons
-        MOVE_TO_FOLDER(MaterialDesignF.FILE_SEND),
-        MOVE_TO_USER_FOLDER(MaterialDesignF.FILE_PDF),
-// -------------------------------------------------------------
+// -------------------- Directory Icons ------------------------------------------------------------------
+        MOVE_TO_FOLDER(MaterialDesignE.EARTH),
+        MOVE_TO_USER_FOLDER(MaterialDesignH.HUMAN),
+// -------------------------------------------------------------------------------------------------------
         COPY_TO_FOLDER(MaterialDesignC.CONTENT_COPY),
         RENAME(MaterialDesignR.RENAME_BOX),
         DELETE_FILE(MaterialDesignD.DELETE_FOREVER),
