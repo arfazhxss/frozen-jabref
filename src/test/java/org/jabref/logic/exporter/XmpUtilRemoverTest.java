68bfc43e102ff156047ab84c4d53ac74f1a52dd3
Arfaz Hussain <arfazhussain.zn@gmail.com>
2024-04-04 20:11:21 -0700

    Minor Update #4 Commit by @arfazhxss on Thu 04 Apr 2024

--- src/test/java/org/jabref/logic/exporter/XmpUtilRemoverTest.java
+++ src/test/java/org/jabref/logic/exporter/XmpUtilRemoverTest.java
@@ -1,218 +1,203 @@
-package org.jabref.logic.exporter;
+package org.jabref.logic.xmp;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.collections.FXCollections;
-
-import org.jabref.logic.xmp.XmpPreferences;
-import org.jabref.model.database.BibDatabaseContext;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.jabref.logic.util.io.FileUtil;
 import org.jabref.model.entry.BibEntry;
+import org.jabref.model.entry.Date;
+import org.jabref.model.entry.Month;
+import org.jabref.model.entry.field.Field;
 import org.jabref.model.entry.field.StandardField;
-
+import org.jabref.model.entry.field.UnknownField;
+import org.jabref.model.entry.types.StandardEntryType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
+import javax.xml.transform.TransformerException;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.jabref.logic.xmp.DublinCoreExtractor.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-public class XmpExporterTest {
+public class XmpUtilRemoverTest {
+    private static BibEntry olly2018;
+    private static BibEntry toral2006;
+    private static BibEntry vapnik2000;
+    private XmpPreferences xmpPreferences;
 
-    private Exporter exporter;
-    private final BibDatabaseContext databaseContext = new BibDatabaseContext();
-    private final XmpPreferences xmpPreferences = mock(XmpPreferences.class);
+    private void initBibEntries() {
+        olly2018 = new BibEntry(StandardEntryType.Article);
+        olly2018.setCitationKey("Olly2018");
+        olly2018.setField(StandardField.AUTHOR, "Olly and Johannes");
+        olly2018.setField(StandardField.TITLE, "Stefan's palace");
+        olly2018.setField(StandardField.JOURNAL, "Test Journal");
+        olly2018.setField(StandardField.VOLUME, "1");
+        olly2018.setField(StandardField.NUMBER, "1");
+        olly2018.setField(StandardField.PAGES, "1-2");
+        olly2018.setMonth(Month.MARCH);
+        olly2018.setField(StandardField.ISSN, "978-123-123");
+        olly2018.setField(StandardField.NOTE, "NOTE");
+        olly2018.setField(StandardField.ABSTRACT, "ABSTRACT");
+        olly2018.setField(StandardField.COMMENT, "COMMENT");
+        olly2018.setField(StandardField.DOI, "10/3212.3123");
+        olly2018.setField(StandardField.FILE, ":article_dublinCore.pdf:PDF");
+        olly2018.setField(StandardField.GROUPS, "NO");
+        olly2018.setField(StandardField.HOWPUBLISHED, "online");
+        olly2018.setField(StandardField.KEYWORDS, "k1, k2");
+        olly2018.setField(StandardField.OWNER, "me");
+        olly2018.setField(StandardField.REVIEW, "review");
+        olly2018.setField(StandardField.URL, "https://www.olly2018.edu");
+
+        toral2006 = new BibEntry(StandardEntryType.InProceedings);
+        toral2006.setField(StandardField.AUTHOR, "Toral, Antonio and Munoz, Rafael");
+        toral2006.setField(StandardField.TITLE, "A proposal to automatically build and maintain gazetteers for Named Entity Recognition by using Wikipedia");
+        toral2006.setField(StandardField.BOOKTITLE, "Proceedings of EACL");
+        toral2006.setField(StandardField.PAGES, "56--61");
+        toral2006.setField(StandardField.EPRINTTYPE, "asdf");
+        toral2006.setField(StandardField.OWNER, "Ich");
+        toral2006.setField(StandardField.URL, "www.url.de");
+
+        vapnik2000 = new BibEntry(StandardEntryType.Book);
+        vapnik2000.setCitationKey("vapnik2000");
+        vapnik2000.setField(StandardField.TITLE, "The Nature of Statistical Learning Theory");
+        vapnik2000.setField(StandardField.PUBLISHER, "Springer Science + Business Media");
+        vapnik2000.setField(StandardField.AUTHOR, "Vladimir N. Vapnik");
+        vapnik2000.setField(StandardField.DOI, "10.1007/978-1-4757-3264-1");
+        vapnik2000.setField(StandardField.OWNER, "Ich");
+        vapnik2000.setField(StandardField.LANGUAGE, "English, Japanese");
+        vapnik2000.setDate(new Date(2000, 5));
+
+        vapnik2000.setField(new UnknownField(DC_COVERAGE), "coverageField");
+        vapnik2000.setField(new UnknownField((DC_SOURCE)), "JabRef");
+        vapnik2000.setField(new UnknownField(DC_RIGHTS), "Right To X");
+    }
 
     @BeforeEach
-    public void setUp() {
-        exporter = new XmpExporter(xmpPreferences);
+    void setUp() {
+        xmpPreferences = mock(XmpPreferences.class);
+        // The code assumes privacy filters to be off
+        when(xmpPreferences.shouldUseXmpPrivacyFilter()).thenReturn(false);
+        when(xmpPreferences.getXmpPrivacyFilter()).thenReturn(FXCollections.observableSet(new HashSet<>()));
+        when(xmpPreferences.getSelectAllFields()).thenReturn(new SimpleBooleanProperty(false));
+        when(xmpPreferences.getKeywordSeparator()).thenReturn(',');
+
+        this.initBibEntries();
     }
 
+    /**
+     * Delete AUTHOR field from XMP metadata that corresponds one entry.
+     */
     @Test
-    public void exportSingleEntry(@TempDir Path testFolder) throws Exception {
-        Path file = testFolder.resolve("ThisIsARandomlyNamedFile");
-        Files.createFile(file);
+    void testDeleteXmpSingleField() throws IOException, URISyntaxException, TransformerException {
+        Path tempFile = Files.createTempFile("JabRef", "pdf");
+        Path pathPdf = Path.of((XmpUtilShared.class.getResource("PD_metadata.pdf").toURI()));
+        // copy data to temp file for testing
+        FileUtil.copyFile(pathPdf, tempFile, true);
 
-        BibEntry entry = new BibEntry();
-        entry.setField(StandardField.AUTHOR, "Alan Turing");
+        // entries in PDF
+        XmpUtilReader reader = new XmpUtilReader();
+        List<BibEntry> originalEntries = reader.readXmp(Path.of(pathPdf.toAbsolutePath().toString()), xmpPreferences);
+        BibEntry expectedEntry = originalEntries.getFirst();
 
-        exporter.export(databaseContext, file, Collections.singletonList(entry));
-        String actual = String.join("\n", Files.readAllLines(file)); // we are using \n to join, so we need it in the expected string as well, \r\n would fail
-        String expected = """
-                  <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">
-                    <rdf:Description xmlns:dc="http://purl.org/dc/elements/1.1/" rdf:about="">
-                      <dc:creator>
-                        <rdf:Seq>
-                          <rdf:li>Alan Turing</rdf:li>
-                        </rdf:Seq>
-                      </dc:creator>
-                      <dc:format>application/pdf</dc:format>
-                      <dc:type>
-                        <rdf:Bag>
-                          <rdf:li>Misc</rdf:li>
-                        </rdf:Bag>
-                      </dc:type>
-                    </rdf:Description>
-                  </rdf:RDF>
-                """.stripTrailing();
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void writeMultipleEntriesInASingleFile(@TempDir Path testFolder) throws Exception {
-        Path file = testFolder.resolve("ThisIsARandomlyNamedFile");
-        Files.createFile(file);
-
-        BibEntry entryTuring = new BibEntry();
-        entryTuring.setField(StandardField.AUTHOR, "Alan Turing");
-
-        BibEntry entryArmbrust = new BibEntry();
-        entryArmbrust.setField(StandardField.AUTHOR, "Michael Armbrust");
-        entryArmbrust.setCitationKey("Armbrust2010");
-
-        exporter.export(databaseContext, file, Arrays.asList(entryTuring, entryArmbrust));
-
-        String actual = String.join("\n", Files.readAllLines(file)); // we are using \n to join, so we need it in the expected string as well, \r\n would fail
-
-        String expected = """
-                    <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">
-                      <rdf:Description xmlns:dc="http://purl.org/dc/elements/1.1/" rdf:about="">
-                        <dc:creator>
-                          <rdf:Seq>
-                            <rdf:li>Alan Turing</rdf:li>
-                          </rdf:Seq>
-                        </dc:creator>
-                        <dc:format>application/pdf</dc:format>
-                        <dc:type>
-                          <rdf:Bag>
-                            <rdf:li>Misc</rdf:li>
-                          </rdf:Bag>
-                        </dc:type>
-                      </rdf:Description>
-                      <rdf:Description xmlns:dc="http://purl.org/dc/elements/1.1/" rdf:about="">
-                        <dc:creator>
-                          <rdf:Seq>
-                            <rdf:li>Michael Armbrust</rdf:li>
-                          </rdf:Seq>
-                        </dc:creator>
-                        <dc:relation>
-                          <rdf:Bag>
-                            <rdf:li>bibtex/citationkey/Armbrust2010</rdf:li>
-                          </rdf:Bag>
-                        </dc:relation>
-                        <dc:format>application/pdf</dc:format>
-                        <dc:type>
-                          <rdf:Bag>
-                            <rdf:li>Misc</rdf:li>
-                          </rdf:Bag>
-                        </dc:type>
-                      </rdf:Description>
-                    </rdf:RDF>
-                  """.stripTrailing();
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void writeMultipleEntriesInDifferentFiles(@TempDir Path testFolder) throws Exception {
-        // set path to the one where the exporter produces several files
-        Path file = testFolder.resolve(XmpExporter.XMP_SPLIT_DIRECTORY_INDICATOR);
-        Files.createFile(file);
-
-        BibEntry entryTuring = new BibEntry()
-                .withField(StandardField.AUTHOR, "Alan Turing");
-        BibEntry entryArmbrust = new BibEntry()
-                .withField(StandardField.AUTHOR, "Michael Armbrust")
-                .withCitationKey("Armbrust2010");
-
-        exporter.export(databaseContext, file, List.of(entryTuring, entryArmbrust));
-
-        // Nothing written in given file
-        List<String> lines = Files.readAllLines(file);
-        assertEquals(Collections.emptyList(), lines);
-
-        // turing contains the turing entry only
-        Path fileTuring = Path.of(file.getParent().toString(), entryTuring.getId() + "_null.xmp");
-        // we are using \n to join, so we need it in the expected string as well, \r\n would fail
-        String actualTuring = String.join("\n", Files.readAllLines(fileTuring));
-        String expectedTuring = """
-                <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">
-                  <rdf:Description xmlns:dc="http://purl.org/dc/elements/1.1/" rdf:about="">
-                    <dc:creator>
-                      <rdf:Seq>
-                        <rdf:li>Alan Turing</rdf:li>
-                      </rdf:Seq>
-                    </dc:creator>
-                    <dc:format>application/pdf</dc:format>
-                    <dc:type>
-                      <rdf:Bag>
-                        <rdf:li>Misc</rdf:li>
-                      </rdf:Bag>
-                    </dc:type>
-                  </rdf:Description>
-                </rdf:RDF>
-              """.stripTrailing();
-        assertEquals(expectedTuring, actualTuring);
-
-        // armbrust contains the armbrust entry only
-        Path fileArmbrust = Path.of(file.getParent().toString(), entryArmbrust.getId() + "_Armbrust2010.xmp");
-        // we are using \n to join, so we need it in the expected string as well, \r\n would fail
-        String actualArmbrust = String.join("\n", Files.readAllLines(fileArmbrust));
-        String expectedArmbrust = """
-                  <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">
-                    <rdf:Description xmlns:dc="http://purl.org/dc/elements/1.1/" rdf:about="">
-                      <dc:creator>
-                        <rdf:Seq>
-                          <rdf:li>Michael Armbrust</rdf:li>
-                        </rdf:Seq>
-                      </dc:creator>
-                      <dc:relation>
-                        <rdf:Bag>
-                          <rdf:li>bibtex/citationkey/Armbrust2010</rdf:li>
-                        </rdf:Bag>
-                      </dc:relation>
-                      <dc:format>application/pdf</dc:format>
-                      <dc:type>
-                        <rdf:Bag>
-                          <rdf:li>Misc</rdf:li>
-                        </rdf:Bag>
-                      </dc:type>
-                    </rdf:Description>
-                  </rdf:RDF>
-                """.stripTrailing();
-        assertEquals(expectedArmbrust, actualArmbrust);
-    }
-
-    @Test
-    public void exportSingleEntryWithPrivacyFilter(@TempDir Path testFolder) throws Exception {
-        when(xmpPreferences.getXmpPrivacyFilter()).thenReturn(FXCollections.observableSet(Collections.singleton(StandardField.AUTHOR)));
+        // turn filters on
         when(xmpPreferences.shouldUseXmpPrivacyFilter()).thenReturn(true);
+        Set<Field> fields = new HashSet<>();
+        fields.add(StandardField.AUTHOR);
+        // filter author field
+        when(xmpPreferences.getXmpPrivacyFilter()).thenReturn(FXCollections.observableSet(fields));
 
-        Path file = testFolder.resolve("ThisIsARandomlyNamedFile");
-        Files.createFile(file);
+        // delete AUTHOR field from the PDF's XMP metadata
+        XmpUtilRemover.deleteXmp(tempFile, xmpPreferences);
+        List<BibEntry> modifiedEntries = reader.readXmp(Path.of(tempFile.toAbsolutePath().toString()), xmpPreferences);
+        BibEntry modifiedEntry = modifiedEntries.get(0);
 
-        BibEntry entry = new BibEntry()
-                .withField(StandardField.AUTHOR, "Alan Turing");
+        expectedEntry.clearField(StandardField.AUTHOR);
 
-        exporter.export(databaseContext, file, Collections.singletonList(entry));
+        // not comparing temp file and original file
+        expectedEntry.clearField(StandardField.FILE);
+        modifiedEntry.clearField(StandardField.FILE);
 
-        String actual = String.join("\n", Files.readAllLines(file));
-        String expected = """
-                  <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">
-                    <rdf:Description xmlns:dc="http://purl.org/dc/elements/1.1/" rdf:about="">
-                      <dc:format>application/pdf</dc:format>
-                      <dc:type>
-                        <rdf:Bag>
-                          <rdf:li>Misc</rdf:li>
-                        </rdf:Bag>
-                      </dc:type>
-                    </rdf:Description>
-                  </rdf:RDF>
-                """.stripTrailing();
+        assertEquals(expectedEntry, modifiedEntry);
+    }
 
-        assertEquals(expected, actual);
+    /**
+     * Delete all XMP metadata fields in PDF file.
+     */
+    @Test
+    void testDeleteXmpAllFields() throws IOException, URISyntaxException, TransformerException {
+        Path tempFile = Files.createTempFile("JabRef", "pdf");
+        Path pathPdf = Path.of((XmpUtilShared.class.getResource("PD_metadata.pdf").toURI()));
+        // copy data to temp file for testing
+        FileUtil.copyFile(pathPdf, tempFile, true);
+
+        // turn filter on
+        when(xmpPreferences.shouldUseXmpPrivacyFilter()).thenReturn(true);
+        // turn select-all-fields on
+        when(xmpPreferences.getSelectAllFields()).thenReturn(new SimpleBooleanProperty(true));
+
+        // delete all XMP metadata fields
+        XmpUtilReader reader = new XmpUtilReader();
+        XmpUtilRemover.deleteXmp(tempFile, xmpPreferences);
+        List<BibEntry> modifiedEntries = reader.readXmp(Path.of(tempFile.toAbsolutePath().toString()), xmpPreferences);
+
+        // since all fields have been deleted, there should be no entry info in the metadata
+        assertEquals(0, modifiedEntries.size());
+    }
+
+    /**
+     * Delete AUTHOR field from all entries in the PDF file's XMP metadata.
+     */
+    @Test
+    void testDeleteXmpWithMultipleEntries(@TempDir Path tempDir) throws IOException, TransformerException {
+        List<BibEntry> entries = Arrays.asList(olly2018, vapnik2000, toral2006);
+        Path tempFile = this.createDefaultFile("banana.pdf", tempDir);
+        XmpPreferences xmpPreferences = mock(XmpPreferences.class);
+        XmpUtilWriter xmpUtilWriter = new XmpUtilWriter(xmpPreferences);
+        xmpUtilWriter.writeXmp(Path.of(tempFile.toAbsolutePath().toString()), entries, null);
+
+        XmpUtilReader reader = new XmpUtilReader();
+        List<BibEntry> expectedEntries = reader.readXmp(Path.of(tempFile.toAbsolutePath().toString()), xmpPreferences);
+
+        // turn filters on
+        when(xmpPreferences.shouldUseXmpPrivacyFilter()).thenReturn(true);
+        Set<Field> fields = new HashSet<>();
+        fields.add(StandardField.AUTHOR);
+        // filter author field
+        when(xmpPreferences.getXmpPrivacyFilter()).thenReturn(FXCollections.observableSet(fields));
+        when(xmpPreferences.getSelectAllFields()).thenReturn(new SimpleBooleanProperty(true));
+        // delete all AUTHOR field from all entries in metadata
+        XmpUtilRemover.deleteXmp(tempFile, xmpPreferences);
+        List<BibEntry> modifiedEntries = reader.readXmp(Path.of(tempFile.toAbsolutePath().toString()), xmpPreferences);
+
+        for (int i = 0; i < modifiedEntries.size(); i++) {
+            expectedEntries.get(i).clearField(StandardField.AUTHOR);
+            expectedEntries.get(i).clearField(StandardField.FILE);
+            modifiedEntries.get(i).clearField(StandardField.FILE);
+            assertEquals(expectedEntries.get(i), modifiedEntries.get(i));
+        }
+    }
+
+    private Path createDefaultFile(String fileName, Path tempDir) throws IOException {
+        // create a default PDF
+        Path pdfFile = tempDir.resolve(fileName);
+        try (PDDocument pdf = new PDDocument()) {
+            // Need a single page to open in Acrobat
+            pdf.addPage(new PDPage());
+            pdf.save(pdfFile.toAbsolutePath().toString());
+        }
+
+        return pdfFile;
     }
 }
