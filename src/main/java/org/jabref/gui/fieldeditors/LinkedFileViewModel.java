5ca5577d5f82defd88424a1f7c519470ab130992
dhuruvan13 <145606952+dhuruvan13@users.noreply.github.com>
2024-04-07 22:05:11 -0700

    Update LinkedFileViewModel.java

--- src/main/java/org/jabref/gui/fieldeditors/LinkedFileViewModel.java
+++ src/main/java/org/jabref/gui/fieldeditors/LinkedFileViewModel.java
@@ -346,7 +346,7 @@ public class LinkedFileViewModel extends AbstractViewModel {
             // Found the linked file, so move it
             try {
                 // Move the file to the user-specific directory
-//                linkedFileHandler.moveToUserSpecificDirectory();
+                linkedFileHandler.moveToUserSpecificDirectory();
                 Files.move(file.get(), userDir.get().resolve(file.get().getFileName()));
             } catch (IOException exception) {
                 dialogService.showErrorDialogAndWait(
